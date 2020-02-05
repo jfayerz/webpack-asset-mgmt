1f1d90f67211013213001af8ff6cc5b86019f73f
jonathan ayers <jfayerz@icloud.com>
2020-02-04 19:53:03 -0500

    added lodash and built webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import _ from 'lodash'; 
+import './style.css';
 
+function component() {
+	const element = document.createElement('div');
+
+	//Lodash, currently included via a script in the html header, is required for this line to work
+	element.innerHTML = _.join(['Hello', 'webpack', 'How\'s it going?'], ' ');
+	element.classList.add('hello');
+
+	return element;
+}
+
+document.body.appendChild(component());
